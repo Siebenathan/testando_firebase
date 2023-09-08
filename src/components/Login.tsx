d2ff827d01e690e384e606213d442f9d2b0c71fa
Siebenathan <118611798+Siebenathan@users.noreply.github.com>
2023-09-08 20:15:41 -0300

    2 files modified, 1 added

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
+import from './Login.module.css';
+
 export default function Login() {
+  const [usuario, setUsuario] = useState<string>('');
+  const [senha, setSenha] = useState<string>('');
+
+  function enviarFormulario(e) {
+    e.preventDefault();
+    console.log(`${usuario}, ${senha}`);
+  }
+
   return (
-    <form>
+    <form onSubmit={enviarFormulario}>
       <div>
         <label htmlFor="userName">Usuario: </label>
         <input
           type="text"
           id="userName"
           name="userName"
+          value={usuario}
           placeholder="Digite seu nome de usuário..."
+          onChange={(e) => setUsuario(e.target.value)}
         />
       </div>
       <div>
@@ -16,9 +29,12 @@ export default function Login() {
           type="password"
           id="userPassword"
           name="userPassword"
+          value={senha}
           placeholder="Digite sua senha..."
+          onChange={(e) => setSenha(e.target.value)}
         />
       </div>
+      <button type="submit">Enviar Formulário</button>
     </form>
   );
 }
